d5d1412796e6aa379f7e1d88452fb78479d2c26c
Tiago Gomes <tidgomes@gmail.com>
2023-01-04 09:04:31 -0300

    refactor sumOfRange

--- cap05/01-sumOfRange.js
+++ cap05/01-sumOfRange.js
@@ -1,18 +1,23 @@
-const range = (start, end) => {
+const range = (start, end, step = 1) => {
   let array = []
-  for(let i = start; i <= end; i++) array.push(i)
+  if(start <= end) for (let i = start; i <= end; i += step) array.push(i)
+  else for (let i = start; i >= end; i += step) array.push(i)
   return array
 }
 
-const sum = (array) => {
+const sum = array => {
   let sum = 0
-  for(let number of array) sum += number
+  for (let number of array) sum += number
   return sum
 }
 
 //Test range:
-console.log(range(1,10))
+console.log(range(1, 10))
+console.log(range(1, 10, 2))
+console.log(range(5, 2, -1))
 
 //Test sum:
-const arrayOfNumbers = range(1,10)
-console.log(sum(arrayOfNumbers))
+console.log('\n\nrange(1, 10) ',sum(range(1, 10)))
+console.log('\n\nrange(1, 10, 2) ',sum(range(1, 10, 2)))
+console.log('\n\nrange(5, 2, -1) ',sum(range(5, 2, -1)))
+console.log('\n\nrange(5, 2, -2) ',sum(range(5, 2, -2)))
